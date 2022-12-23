17a8883c0ca973f542da6c1b0b6d2e949625168a
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-12-23 12:14:03 +0700

    Add icons and PWA manifest

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,37 @@ import solidPlugin from 'vite-plugin-solid';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [solidPlugin(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'PromptPay QR Generator',
+        short_name: 'PromptPay QR',
+        description: 'Generate PromtPay QR Codes with prefilled amount',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'mask-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000,
   },
