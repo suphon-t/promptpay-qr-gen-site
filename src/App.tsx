fc957a84f8b5e1cc28e366c380901e1d6aee6c58
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-12-23 12:34:57 +0700

    Rework validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { Component, createMemo } from 'solid-js';
+import { Component, createMemo, Show } from 'solid-js';
 import { createAppStore } from './store';
 import { generate } from 'promptparse/dist/generators/promptpay/AnyID';
 import { QRCode } from './QRCode';
 import { Input } from './Input';
 
-function parseAmount(amountString: string) {
-  if (amountString === '') return undefined;
-  const amount = Number(amountString);
-  if (!isFinite(amount)) throw new Error('Invalid amount');
-  return amount;
-}
+const promptpayIdPattern = /\d{8,15}/;
+const amountPattern = /d*/;
 
 const App: Component = () => {
   const [state, setState] = createAppStore();
 
   const qr = createMemo(() => {
-    try {
-      const amount = parseAmount(state.amount);
-      return generate(state.promptpayId, amount);
-    } catch (e) {
-      return null;
-    }
+    if (!promptpayIdPattern.test(state.promptpayId)) return null;
+    if (!amountPattern.test(state.amount)) return null;
+    return generate(state.promptpayId, Number(state.amount));
   });
 
-  const amountHasError = createMemo(() => qr() === null);
+  const promptpayIdHasError = createMemo(
+    () => !promptpayIdPattern.test(state.promptpayId)
+  );
+  const amountHasError = createMemo(() => !amountPattern.test(state.amount));
 
   return (
     <div class="max-w-[500px] mx-auto p-4">
       <h1 class="text-3xl font-bold mb-4">PromptPay QR Generator</h1>
       <Input
-        type="tel"
+        type="number"
+        pattern="\\d*"
+        placeholder="PromptPay ID"
+        hasError={promptpayIdHasError()}
         value={state.promptpayId}
         onInput={(e) => setState('promptpayId', e.currentTarget.value)}
       />
@@ -37,12 +36,15 @@ const App: Component = () => {
         class="mt-4"
         type="number"
         pattern="\\d*"
+        placeholder="Amount"
+        hasError={amountHasError()}
         value={state.amount}
         onInput={(e) => setState('amount', e.currentTarget.value)}
-        hasError={amountHasError()}
       />
       <div class="mt-4">
-        <QRCode qrData={qr} />
+        <Show when={qr() !== null}>
+          <QRCode qrData={qr} />
+        </Show>
       </div>
     </div>
   );
