953f49bd4b00aa343dfe1a66f6e0f1f9f2cd844d
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-12-23 14:18:09 +0700

    Hide id input on blur

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component, createMemo, Show } from 'solid-js';
+import { Component, createMemo, createSignal, Show } from 'solid-js';
 import { createAppStore } from './store';
 import { generate } from 'promptparse/dist/generators/promptpay/AnyID';
 import { QRCode } from './QRCode';
@@ -9,6 +9,9 @@ const amountPattern = /d*/;
 
 const App: Component = () => {
   const [state, setState] = createAppStore();
+  const [editing, setEditing] = createSignal(
+    !promptpayIdPattern.test(state.promptpayId)
+  );
 
   const qr = createMemo(() => {
     if (!promptpayIdPattern.test(state.promptpayId)) return null;
@@ -24,14 +27,36 @@ const App: Component = () => {
   return (
     <div class="max-w-[500px] mx-auto p-4">
       <h1 class="text-3xl font-bold mb-4">PromptPay QR Generator</h1>
-      <Input
-        type="number"
-        pattern="\\d*"
-        placeholder="PromptPay ID"
-        hasError={promptpayIdHasError()}
-        value={state.promptpayId}
-        onInput={(e) => setState('promptpayId', e.currentTarget.value)}
-      />
+      <Show
+        when={editing()}
+        fallback={
+          <p>
+            <span class="text-xl">
+              To: <span class="font-semibold">{state.promptpayId}</span>
+            </span>{' '}
+            <button
+              class="text-blue-800 underline"
+              onClick={() => setEditing(true)}
+            >
+              Edit
+            </button>
+          </p>
+        }
+      >
+        <Input
+          type="number"
+          pattern="\\d*"
+          placeholder="PromptPay ID"
+          hasError={promptpayIdHasError()}
+          value={state.promptpayId}
+          autofocus
+          onInput={(e) => setState('promptpayId', e.currentTarget.value)}
+          onBlur={() => {
+            if (promptpayIdHasError()) return;
+            setEditing(false);
+          }}
+        />
+      </Show>
       <Input
         class="mt-4"
         type="number"
